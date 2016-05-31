edc8b49d0f4d9ee21c47391b6d5f569acb6d07b6
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-31 15:06:25 +0100

    Implements top_up functionality

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,5 +1,16 @@
 class OysterCard
 
+  MAX_BALANCE = 90
+
   attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end 
+
+  def top_up(value)
+    fail('Balance limit reached!') if @balance + value > MAX_BALANCE
+    @balance += value
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,29 @@
 describe OysterCard do
+  subject(:oyster_card) { described_class.new }
+
+  context 'At initialization' do
+    it 'Has with a balance of zero' do
+      expect(oyster_card.balance).to eq 0
+    end
+  end
 
   context 'In any context' do
 
     it { is_expected.to respond_to(:balance) }
 
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    describe '#top_up' do
+ 
+      it 'adds value to balance' do
+        expect{ oyster_card.top_up(1) }.to change{ oyster_card.balance }.by 1
+      end
+
+      it 'raises error if balance reaches limit' do
+        oyster_card.top_up(OysterCard::MAX_BALANCE)
+        expect{ oyster_card.top_up(1) }.to raise_error('Balance limit reached!') 
+      end
+    end
   end 
 
 end
