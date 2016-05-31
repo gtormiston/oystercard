ff30058dedba3a51338531ef62f4faed457f7e2b
Carlos <chdezmar@gmail.com>
2016-05-31 16:09:46 +0100

    Introducing #touch_in and #touch_out

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,16 +1,36 @@
 class OysterCard
 
-  MAX_BALANCE = 90
+  MAX_BALANCE = 90 # => pounds
 
   attr_reader :balance
 
-  def initialize
-    @balance = 0
-  end 
+  def initialize()
+    @balance = 0 # => pounds
+    @in_journey = false
+  end
 
   def top_up(value)
-    fail('Balance limit reached!') if @balance + value > MAX_BALANCE
+    fail("Balance limit of #{MAX_BALANCE} reached") if @balance + value > MAX_BALANCE
     @balance += value
   end
+
+  def deduct(value)
+    @balance -= value
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,25 +5,93 @@ describe OysterCard do
     it 'Has with a balance of zero' do
       expect(oyster_card.balance).to eq 0
     end
+
+    it "#in_journey? returns false" do
+      expect(oyster_card).not_to be_in_journey
+    end
+
   end
 
   context 'In any context' do
 
     it { is_expected.to respond_to(:balance) }
 
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     describe '#top_up' do
- 
+
       it 'adds value to balance' do
         expect{ oyster_card.top_up(1) }.to change{ oyster_card.balance }.by 1
       end
 
       it 'raises error if balance reaches limit' do
-        oyster_card.top_up(OysterCard::MAX_BALANCE)
-        expect{ oyster_card.top_up(1) }.to raise_error('Balance limit reached!') 
+        max_balance = OysterCard::MAX_BALANCE
+        oyster_card.top_up(max_balance)
+        expect{ oyster_card.top_up(1) }.to raise_error("Balance limit of #{max_balance} reached")
       end
     end
-  end 
+
+    describe '#deduct' do
+
+      it 'deducts value from balance' do
+        oyster_card.top_up(10)
+        expect{ oyster_card.deduct(1) }.to change{ oyster_card.balance }.by -1
+      end
+
+      # it 'raises error if balance reaches limit' do
+      #   max_balance = OysterCard::MAX_BALANCE
+      #   oyster_card.top_up(max_balance)
+      #   expect{ oyster_card.top_up(1) }.to raise_error("Balance limit of #{max_balance} reached")
+      # end
+    end
+  end
+
+  describe '#touch_in' do
+    context "When oyster_card is in journey" do
+      it "changes journey status to true" do
+        oyster_card.touch_in
+        expect(oyster_card).to be_in_journey
+      end
+
+
+    end
+    # context "When oyster_card is not in journey" do
+    #   it "raises an error"do
+    #
+    #   end
+    # end
+
+    describe '#touch_out' do
+      context "When oyster_card is in journey" do
+        it "changes journey status to false" do
+          oyster_card.touch_in
+          oyster_card.touch_out
+          expect(oyster_card).not_to be_in_journey
+        end
+      end
+    end
+
+
+
+
+
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
